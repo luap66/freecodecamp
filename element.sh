5ce4cc28b0930f1d5339170a542ea74d30177b9a
Paul Waldhoff <paul.florian.waldhoff@gmail.com>
2025-03-22 23:09:07 +0000

    fix: asd

---
+++ element.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# Überprüfen, ob mindestens ein Argument übergeben wurde
+if [ $# -lt 1 ]; then
+  echo "Please provide an element as an argument."
+
+
+else 
+# Prüfen, ob das erste Argument eine Zahl ist
+if [[ "$1" =~ ^[0-9]+$ ]]; then
+ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE atomic_number='$1'")
+else
+  ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1'")
+    if [ -z "$ATOMIC_NUMBER" ]; then
+      ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")
+    fi
+fi
+
+if [ -z "$ATOMIC_NUMBER" ]; then
+echo I could not find that element in the database.
+
+else
+SYMBOL=$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
+NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER")
+TYPE=$($PSQL "SELECT type FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+ATOMIC_MASS=$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+MELTING_POINT=$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+BOILING_POINT=$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+
+fi
+fi
